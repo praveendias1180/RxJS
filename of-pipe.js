443425f2d4fcf0bfef5f0b8c7b7a222f56529813
Praveen Dias <praveendias1180@gmail.com>
2022-04-14 10:12:20 +0530

    of and pipe operators

---
+++ of-pipe.js
@@ -0,0 +1,20 @@
+const { of } =require('rxjs');
+const { map, filter } =require('rxjs/operators');
+/*
+ *  'of' allows you to deliver values in a sequence
+ *  In this case, it will emit 1,2,3,4,5 in order.
+ */
+const dataSource = of(1, 2, 3, 4, 5);
+
+// subscribe to our source observable
+const subscription = dataSource
+  .pipe(
+    // only accept values 2 or greater
+    filter(value => value >= 2)
+  )
+  .pipe(
+    // add 1 to each emitted value
+    map(value => value + 1)
+  )
+  // log: 2, 3, 4, 5, 6
+  .subscribe(value => console.log(value));
